f473c13e0bac47d8e0a20d845baca1cf63985813
raceimaztion <dvanhumb@sfu.ca>
2009-03-11 17:40:33 -0700

    Started the launch framework for the client and the server.

---
+++ Sphereority2.java
@@ -0,0 +1,13 @@
+
+/**
+ * The main entry class for the client.
+ * This gets called from the command-line.
+ * @author dvanhumb
+ */
+public class Sphereority2
+{
+	public static void main(String[] args)
+	{
+		// TODO: Parse parameters and launch clients
+	}
+}
---
+++ Sphereority2Server.java
@@ -0,0 +1,15 @@
+
+/**
+ *   The main entry method for the server.
+ *   This can be called from the command-line
+ * or from the client library when the user
+ * wants a local server of their own. 
+ * @author dvanhumb
+ */
+public class Sphereority2Server
+{
+	public static void main(String[] args)
+	{
+		// TODO: Parse parameters and launch server classes
+	}
+}
